8974cb33f20b3c142d5a5a4a7c40051e7d00cf1e
luis-yampa <luiseduardo@yampa.com.br>
2025-01-05 18:24:14 -0300

    adiciona rotas e lógica para gerenciamento de visualizações no componente de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,28 @@ export const routes: Routes = [
   },
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent
+      },
+      {
+        path: 'forgot-password',
+        component: LoginComponent
+      },
+      {
+        path: 'verification-code',
+        component: LoginComponent
+      },
+      {
+        path: 'reset-password',
+        component: LoginComponent
+      },
+      {
+        path: 'sign-up',
+        component: LoginComponent
+      }
+    ]
   }
 ];
