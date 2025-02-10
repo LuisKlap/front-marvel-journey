a4c24b873423dbe878e4596a93d98277c5019a2a
LuisKlap <luis_klap@outlook.com>
2025-02-10 20:37:26 -0300

    refatora estrutura do layout, movendo componentes de menu para o componente Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { TopMenuComponent } from './features/shared/components/top-menu/top-menu.component';
-import { SidebarComponent } from './features/shared/components/sidebar/sidebar.component';
 import { AuthService } from './features/auth/service/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, TopMenuComponent, SidebarComponent],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/features/home/components/home/home.component.ts
+++ src/app/features/home/components/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { TopMenuComponent } from '../../../shared/components/top-menu/top-menu.component';
+import { SidebarComponent } from '../../../shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [TopMenuComponent, SidebarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   standalone: true
